0094a0627296ca3fdffaa1d538bc07b0e114b185
endsun1234 <endsun1234@naver.com>
2025-10-10 16:17:06 +0900

    Skill 시스템 수정
    성호님이랑 map-generate 브랜치에서 스킬 시스템 관련 스크립트 수정
    
    Co-Authored-By: whtjdgh1931 <175549264+whtjdgh1931@users.noreply.github.com>

--- Assets/02_Scripts/Map_Generate/Room/NormalRoom.cs
+++ Assets/02_Scripts/Map_Generate/Room/NormalRoom.cs
@@ -1,4 +1,17 @@
+using UnityEngine;
+
 public class NormalRoom : Room
 {
-    
+    public SkillBase skillCompany;
+
+    private void Start()
+    {
+        SetSkillCompany();
+    }
+
+    public void SetSkillCompany()
+    {
+        skillCompany= SkillManager.Instance().DrawSkillCompany();
+        Debug.Log(skillCompany.skillBaseString);
+    }
 }
--- Assets/02_Scripts/Skill/SkillBase.cs
+++ Assets/02_Scripts/Skill/SkillBase.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class SkillBase : MonoBehaviour
 {
-    protected abstract string skillBaseString { get; set; }
+    public string skillBaseString;
 
 
     /// <summary>
