43eac4c88809e06000a03a031433c0ed7aeb0ce7
endsun1234 <endsun1234@naver.com>
2025-10-10 10:19:22 +0900

    IInteractable 인터페이스 설명 추가

--- Assets/02_Scripts/Interface/IInteractable.cs
+++ Assets/02_Scripts/Interface/IInteractable.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 /// <summary>
 /// 상호작용 가능한 인터페이스
+/// 이 인터페이스를 구현하는 오브젝트는 플레이어와 상호작용할 수 있습니다.
+/// Interactable 오브젝트는 플레이어가 접근했을 때 상호작용 가이드를 표시하고,
+/// InteractableDetector 스크립트를 통해 상호작용을 처리합니다.
 /// </summary>
 public interface IInteractable
 {
