5def0f62201257e9eb3619451c9bb48185bb1a87
whtjdgh1931 <pmkjo1931@gmail.com>
2025-10-10 10:55:17 +0900

    feature : skillChoose fix
    스킬 선택 메서드 수정, 업그레이드 메서드 수정

--- Assets/02_Scripts/Skill/Skill_Three.cs
+++ Assets/02_Scripts/Skill/Skill_Three.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class Skill_Three : SkillBase
 {
-		public override void ActivateSkill(SkillData choosedSkill)
-		{
-				throw new System.NotImplementedException();
-		}
+    public override void ActivateSkill(SkillData choosedSkill)
+    {
+        Debug.Log("3번 회사 스킬 선택");
+    }
 }
