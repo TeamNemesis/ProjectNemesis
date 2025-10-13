2cb47f62cac8b3e8ab4135083c30d2227d9e1a9c
endsun1234 <endsun1234@naver.com>
2025-10-13 10:07:12 +0900

    Room->Door로 이름을 명확히함
    다음 '문'을 생성하는 역할이고
    클래스들도 문과 관련된 역할이므로
    용어 수정

---
+++ Assets/02_Scripts/Interaction/InteractableObjects/Door/BossDoor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BossDoor : Door
+{
+    [SerializeField] string _doorName = "bossDoor";
+    [SerializeField] float _doorChance = 0.05f; // 보스룸의 등장 확률 설정
+    [SerializeField] DoorType _doorType = DoorType.Boss;
+
+    public override string DoorName => _doorName;
+
+    public override float DoorChance => _doorChance;
+
+    public override DoorType DoorType => _doorType;
+
+    public override void Initialize()
+    {
+        //Debug.Log("보스룸 생성됨");
+    }
+}
---
+++ Assets/02_Scripts/Interaction/InteractableObjects/Door/StartDoor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StartDoor : Door
+{
+    [SerializeField] string _doorName = "startDoor";
+    [SerializeField] float _doorChance = 1.0f; // 시작룸의 등장 확률 설정
+    [SerializeField] DoorType _doorType = DoorType.Start;
+
+    public override string DoorName => _doorName;
+    public override float DoorChance => _doorChance;
+    public override DoorType DoorType => _doorType;
+
+    public override void Initialize()
+    {
+        //Debug.Log("시작룸 생성됨");
+    }
+}
