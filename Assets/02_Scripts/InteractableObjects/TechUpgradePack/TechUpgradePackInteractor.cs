600e8e3cdefd4591c746582a49d5aa5bf7373441
endsun1234 <endsun1234@naver.com>
2025-10-15 03:24:27 +0900

    플레이어 컴포넌트들 작업중
    플레이어의 기능을 하나하나 컴포넌트로 나눠서 작업중
    WeaponController를 통해 무기를 바꾸는 시스템을 만듦

--- Assets/02_Scripts/InteractableObjects/TechUpgradePack/TechUpgradePackInteractor.cs
+++ Assets/02_Scripts/InteractableObjects/TechUpgradePack/TechUpgradePackInteractor.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 
-
 public class TechUpgradePackInteractor : MonoBehaviour, IInteractable
 {
     [SerializeField] Transform _guidePoint;
--- Assets/02_Scripts/InteractableObjects/Weapon/WeaponInteractor.cs
+++ Assets/02_Scripts/InteractableObjects/Weapon/WeaponInteractor.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class WeaponInteractor : MonoBehaviour, IInteractable
 {
     [SerializeField] Transform _guidePoint;
+    [SerializeField] WeaponType _weaponType;
 
     public Vector3 GuidePoint => _guidePoint.position;
 
     public InteractableType InteractableType => InteractableType.Weapon;
+    public WeaponType WeaponType => _weaponType;
+
+    public event Action<WeaponType> OnWeaponInteracted;
 
     public void Interact(Transform subject)
     {
         Debug.Log("무기와 상호작용 함");
+        OnWeaponInteracted?.Invoke(_weaponType);
     }
 }
