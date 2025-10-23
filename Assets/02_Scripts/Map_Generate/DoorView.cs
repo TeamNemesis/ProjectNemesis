4d08b35c299090ebd5aa6d93848f9caafbf88603
endsun1234 <endsun1234@naver.com>
2025-10-23 13:29:22 +0900

    문에 보상획득 여부 플래그 추가

--- Assets/02_Scripts/Map_Generate/DoorView.cs
+++ Assets/02_Scripts/Map_Generate/DoorView.cs
@@ -8,8 +8,30 @@ public class DoorView : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _rewardText;
 
-    public void SetReward(Room room)
+    public void SetReward(RoomInfo roomInfo)
     {
+        // 방의 정보에 따라 보상 결정
+        if (roomInfo.TryGetTechSelect(out var tech))
+        {
+            // TechSelect 우선 처리
+            _rewardText.text = $"Tech: {tech.ToString()}";
+        }
+        else if (roomInfo.TryGetNormal(out var normal))
+        {
+            // Normal 처리
+            _rewardText.text = $"Normal: {normal.ToString()}";
+        }
+        else
+        {
+            // RoomType 처리
+            var roomType = roomInfo.RoomType;
+            _rewardText.text = $"Room: {roomType.ToString()}";
+        }
 
+    }
+
+    public void ToggleReward(bool isOn)
+    {
+        _rewardText.gameObject.SetActive(isOn);
     }
 }
