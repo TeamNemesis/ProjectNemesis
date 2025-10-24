407ee07e7845b962216f46209ceb16e22efd3200
whtjdgh1931 <whtjdgh1931@naver.com>
2025-10-24 11:14:25 +0900

    feature : player 체력 변경 이벤트 변경

--- Assets/02_Scripts/Player/PlayerModel.cs
+++ Assets/02_Scripts/Player/PlayerModel.cs
@@ -89,7 +89,6 @@ public class PlayerModel : CharacterModelBase
     {
         base.Initialize();
         SetCurrentHp(maxHealth); // 초기화 시 현재 체력을 최대 체력으로 설정
-        OnHpChangedEventPlay(currentHealth); // 초기 체력 이벤트 발행
         debuffHandler.InitializePlayer();
     }
 
