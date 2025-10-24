d1a9a4cb27eda118f199e5ab28d7ae8232a2de79
endsun1234 <endsun1234@naver.com>
2025-10-24 16:09:35 +0900

    보상 프리펩및 데이터화중

--- Assets/02_Scripts/InteractableObjects/TechUpgradePack/TechUpgradePackInteractor.cs
+++ Assets/02_Scripts/InteractableObjects/TechUpgradePack/TechUpgradePackInteractor.cs
@@ -1,19 +1,6 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class TechUpgradePackInteractor : InteractableObject
+public class TechUpgradePackInteractor : MonoBehaviour
 {
-    [SerializeField] Transform _guidePoint;
 
-    public override Vector3 GuidePoint => _guidePoint.position;
-
-    public override InteractableType InteractableType => InteractableType.Reward;
-
-    public override event Action<IInteractable> OnInteracted;
-
-    public override void StartInteract(Transform subject)
-    {
-        Debug.Log("업그레이드 팩과 상호작용 함");
-        OnInteracted?.Invoke(this);
-    }
 }
