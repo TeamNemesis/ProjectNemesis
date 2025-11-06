14730101638438453a2e36ee884c6464e1e53f23
djqfhemdyd159 <djqfhemdyd159@gmail.com>
2025-11-06 09:52:42 +0900

    몬스터 스포너 보스 추가

--- Assets/02_Scripts/Monster/Boss/BossAddOn/IceTank.cs
+++ Assets/02_Scripts/Monster/Boss/BossAddOn/IceTank.cs
@@ -2,30 +2,9 @@ using UnityEngine;
 
 public class IceTank : MonsterBase
 {
-    private void Update()
-    {
-        if (isDead || _target == null) return;
-        if (isStunned) return;
-        LookAtPlayer();
-
-        switch (baseState)
-        {
-            case MonsterState.Idle:
-                HandleIdle();
-                break;
-            case MonsterState.Die:
-                Die();
-                break;
-        }
-    }
-
-    private void HandleIdle()
-    {
-
-    }
-
     public void SetStateDie()
     {
         baseState = MonsterState.Die;
+        GameManager.Instance.PoolManager.ReleaseToPool(gameObject);
     }
 }
