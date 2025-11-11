124571e81966cf88922a386f60e9a6cf8499900a
endsun1234 <endsun1234@naver.com>
2025-11-11 11:26:29 +0900

    프리펩 업데이트

--- Assets/02_Scripts/PlayScene/PlaySceneView.cs
+++ Assets/02_Scripts/PlayScene/PlaySceneView.cs
@@ -356,6 +356,11 @@ public class PlaySceneView : MonoBehaviour
         }
     }
 
+    public void OnGoToMainClicked()
+    {
+        GameManager.Instance.SceneLoadManager.LoadIntroScene();
+    }
+
     private void OnDisable()
     {
         EventBus.OnBossDead -= ShowGameClearPanel;
