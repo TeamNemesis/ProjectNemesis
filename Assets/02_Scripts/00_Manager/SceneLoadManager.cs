606b82de1948d89f3bf498c342ba973b1a767f11
endsun1234 <endsun1234@naver.com>
2025-11-12 15:51:20 +0900

    맵 미리로딩 테스트

--- Assets/02_Scripts/00_Manager/SceneLoadManager.cs
+++ Assets/02_Scripts/00_Manager/SceneLoadManager.cs
@@ -16,6 +16,19 @@ public class SceneLoadManager : MonoBehaviour
         SceneManager.LoadScene(Constants.SCENE_NAME_INTRO);
         GameManager.Instance.PoolManager.ClearAllPools();
 
+        // 맵 미리 생성 후 꺼주기
+        GameObject bossRoom = GameManager.Instance.PoolManager.GetFromPool("Prefabs/Map/Rooms/BossRoom", Vector3.zero, Quaternion.identity);
+        GameObject NormalRoom = GameManager.Instance.PoolManager.GetFromPool("Prefabs/Map/Rooms/Normal", Vector3.zero, Quaternion.identity);
+        GameObject ShopRoom = GameManager.Instance.PoolManager.GetFromPool("Prefabs/Map/Rooms/Shop", Vector3.zero, Quaternion.identity);
+        GameObject StartRoom = GameManager.Instance.PoolManager.GetFromPool("Prefabs/Map/Rooms/StartRoom", Vector3.zero, Quaternion.identity);
+        GameObject Colosseum = GameManager.Instance.PoolManager.GetFromPool("Prefabs/Map/Rooms/Colosseum", Vector3.zero, Quaternion.identity);
+        GameObject Laboratory = GameManager.Instance.PoolManager.GetFromPool("Prefabs/Map/Rooms/Laboratory", Vector3.zero, Quaternion.identity);
+        bossRoom.SetActive(false);
+        NormalRoom.SetActive(false);
+        ShopRoom.SetActive(false);
+        StartRoom.SetActive(false);
+        Colosseum.SetActive(false);
+        Laboratory.SetActive(false);
     }
 
     public void LoadPlayScene()
