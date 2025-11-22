4c07e91de91bcfc24313469f438f1b3050a52d85
Yuja0205 <youjaehyun2@gmail.com>
2025-11-22 16:23:45 +0900

    연속피격 화면 해결
    연속피격 화면 해결

--- Assets/02_Scripts/Movement/Damaged.cs
+++ Assets/02_Scripts/Movement/Damaged.cs
@@ -1,29 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class Damaged : MonoBehaviour
 {
     [SerializeField] private Image panelImage;
+    private Coroutine fadeRoutine;
 
     private void Update()
     {
-        // 숫자키 0 = KeyCode.Alpha0
         if (Input.GetKeyDown(KeyCode.Alpha0))
         {
-            StartCoroutine(FadePanel());
+            if (fadeRoutine != null)
+                StopCoroutine(fadeRoutine);
+
+            fadeRoutine = StartCoroutine(FadePanel());
         }
     }
 
     private IEnumerator FadePanel()
     {
-        // 1) 알파 0.3으로 설정
+        // 즉시 알파 0.3
         Color c = panelImage.color;
         c.a = 0.3f;
         panelImage.color = c;
 
-        // 2) 1초 동안 알파를 0까지 줄이기
         float duration = 1f;
         float time = 0f;
 
@@ -38,7 +39,6 @@ public class Damaged : MonoBehaviour
             yield return null;
         }
 
-        // 마지막에 확실하게 알파 0으로
         c.a = 0f;
         panelImage.color = c;
     }
