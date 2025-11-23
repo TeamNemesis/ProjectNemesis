82ecb1065c913504c10d43263983b5c4a7989ccb
whtjdgh1931 <whtjdgh1210@naver.com>
2025-11-23 22:00:51 +0900

    feature : 셰이더 로딩 방식 변경,  데미지 숫자 크기 변경

--- Assets/02_Scripts/ServerData/ServerScene.cs
+++ Assets/02_Scripts/ServerData/ServerScene.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class ServerScene : MonoBehaviour
 {
+
+
     public void Start()
     {
-      
         GameManager.Instance.serverManager.ServerStart();
+  
     }
 
 }
