c36860fd9dab3c58c36e10ffb3a23cd151b06299
endsun1234 <endsun1234@naver.com>
2025-11-25 15:24:41 +0900

    SFX 추가
    DoorOpen, TechSelectPack(임시)

--- Assets/02_Scripts/InteractableObjects/TechSelectPack/TechSelectPackInteractor.cs
+++ Assets/02_Scripts/InteractableObjects/TechSelectPack/TechSelectPackInteractor.cs
@@ -21,6 +21,7 @@ public class TechSelectPackInteractor : RewardInteractableObject
     protected override IEnumerator RewardCoroutine()
     {
         GameManager.Instance.UIManager.onRewardSelect += RaiseRewardGivenEvent;
+        GameManager.Instance.SoundManager.PlaySfx("UIPopUp");
         yield return new WaitForSeconds(0.5f); // 보상 선택 UI 열리는 시간 대기
         _techItem.GetSkill(_packType);
     }
